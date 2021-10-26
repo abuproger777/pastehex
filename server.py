97ed4c45341b7808dc26a78571834b7ce614cb8a
abuproger777 <93068011+abuproger777@users.noreply.github.com>
2021-10-26 14:38:35 +0500

    added view create_post

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import sqlite3
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, redirect, url_for, session
+
+from forms import CreatePostForm
+from validations import validate1
 
 
 app = Flask(__name__)
@@ -20,5 +23,27 @@ def close_connection(exception):
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/create_post", methods=["GET", "POST"])
+def create_post():       
+    form = CreatePostForm()
+
+    if form.submit.data and form.validate():
+        post = form.post.data
+        post = post.replace("{", "&#123;").replace("}", "&#125;")  # No server-side template injection
+
+        db = get_db()
+        cur = db.cursor()
+        is_admin = validate1(session, cur)
+        cur.execute(
+            """
+            INSERT INTO posts (post, is_admin) VALUES (?, ?)
+            """, (post, is_admin)
+            )
+        db.commit()
+
+        return redirect(url_for("all_posts"))
+    return render_template("create_post.html", form=form)
+
 
 app.run("0.0.0.0", debug=True)
